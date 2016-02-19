2ae965562f051a18649c906691e7630578ffe409
blueteck <tili2@gmx.de>
2016-02-19 09:11:10 +0000

    add metadata via form

--- Metadata/Controller/Metadata.php
+++ Metadata/Controller/Metadata.php
@@ -14,38 +14,79 @@ class Metadata extends Base
      *
      * @access public
      */
-    public function project()
+    public function project($id = 0)
     {
-        $project = $this->getProject();
+        if($id == 0){
+            $project = $this->getProject();
+        }else{
+            $project = $this->project->getById($id);
+        }
         
         $metadata = $this->projectMetadata->getAll($project['id']);
         
         $this->response->html($this->helper->layout->project('metadata:metadata/metadata', array('title' => t('Metadata'), 
                                                                                                  'project' => $project,
+                                                                                                 'type' => 'project',
+                                                                                                 'id' => $project['id'],
                                                                                                  'metadata' => $metadata )));
     }
     
-    public function task()
+    public function task($id = 0)
     {
-        $project = $this->getProject();
-        $task = $this->getTask();
+        if($id == 0){
+            $project = $this->getProject();
+            $task = $this->getTask();
+        }else{
+            $task = $this->taskFinder->getById($id);
+            $project = $this->project->getById($task['project_id']);
+        }
         
         $metadata = $this->taskMetadata->getAll($task['id']);
         
         $this->response->html($this->helper->layout->task('metadata:metadata/metadata', array('title' => t('Metadata'), 
                                                                                               'task' => $task,  
                                                                                               'project' => $project,
+                                                                                              'type' => 'task',
+                                                                                              'id' => $task['id'],
                                                                                               'metadata' => $metadata)));
     }
     
-    public function user()
+    public function user($id = 0)
     {
-        $user = $this->getUser();
-        
+        if($id == 0 ){
+            $user = $this->getUser();
+        }else{
+            $user = $this->user->getById($id);
+        }
         $metadata = $this->userMetadata->getAll($user['id']);
         
         $this->response->html($this->helper->layout->user('metadata:metadata/metadata', array('title' => t('Metadata'), 
                                                                                               'user' => $user,
+                                                                                              'type' => 'user',
+                                                                                              'id' => $user['id'],
                                                                                               'metadata' => $metadata)));
+    }
+    
+    public function add()
+    {
+        
+        $values = $this->request->getValues();
+        
+        switch ($values['type']){
+            case 'project':
+                $this->projectMetadata->save($values['id'], [$values['key'] => $values['value']]);
+                $this->response->redirect($this->project($values['id']));
+                break;
+            case 'task':
+                $this->taskMetadata->save($values['id'], [$values['key'] => $values['value']]);
+                $this->response->redirect($this->task($values['id']));
+                break;
+            case 'user':
+                $this->userMetadata->save($values['id'], [$values['key'] => $values['value']]);
+                $this->response->redirect($this->user($values['id']));
+                break;
+        }
+        
+        
     }
 }
--- Metadata/Template/metadata/metadata.php
+++ Metadata/Template/metadata/metadata.php
@@ -8,14 +8,28 @@
 <?php else: ?>
     <table class="table-small table-fixed">
     <tr>
-        <th class="column-50"><?= t('Key') ?></th>
-        <th class="column-50"><?= t('Value') ?></th>
+        <th class="column-40"><?= t('Key') ?></th>
+        <th class="column-40"><?= t('Value') ?></th>
+        <!--<th class="column-20"><?= t('Action') ?></th>-->
     </tr>
     <?php foreach ($metadata as $key => $value): ?>
     <tr>
         <td><?= $key ?></td>
         <td><?= $value ?></td>
+        <!--<td>
+            <ul>
+                    <li>
+                        <?= $this->url->link(t('Edit'), 'metadata', 'edit', array('plugin' => 'metadata'), false, 'popover') ?>
+                    </li>
+                    <li>
+                        <?= $this->url->link(t('Remove'), 'metadata', 'confirm', array('plugin' => 'metadata' ), false, 'popover') ?>
+                    </li>
+                </ul>
+        </td>-->
     </tr>
     <?php endforeach ?>
     </table>
 <?php endif ?>
+
+
+<?= $this->render('metadata:metadata/add', array('type' => $type, 'id' => $id)) ?>
