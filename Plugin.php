5123eb40bdaeaf5e194d2aacd1eab82515d3713d
blueteck <tili2@gmx.de>
2016-03-21 15:59:09 +0000

    V 1.0.1

--- Plugin.php
+++ Plugin.php
@@ -39,7 +39,7 @@ class Plugin extends Base
     }
     public function getPluginVersion()
     {
-        return '1.0.0';
+        return '1.0.1';
     }
     public function getPluginHomepage()
     {
