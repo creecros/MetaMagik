b6d58ef43d0c0da5bb993da08f37a9699902c630
Sébastien NOBILI <pipoprods@free.fr>
2016-03-23 13:17:09 +0100

    French translation

---
+++ Locale/fr_FR/translations.php
@@ -0,0 +1,11 @@
+<?php
+
+return array(
+    'Metadata' => 'Metadonnées',
+    'Key' => 'Clé',
+    'Value' => 'Valeur',
+    'No metadata' => 'Aucune metadonnée',
+    'Remove Metadata' => 'Supprimer une metadonnée',
+    'Do you really want to remove this metadata?' => 'Voulez-vous vraiment supprimer cette metadonnée ?',
+    'Add Metadata' => 'Ajouter une metadonnée',
+);
