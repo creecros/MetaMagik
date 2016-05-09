4fb715b32548e7f31242385fa75f19265ded4488
Timo <tili2@gmx.de>
2016-05-09 21:21:56 +0200

    v1.0.3

--- Plugin.php
+++ Plugin.php
@@ -52,7 +52,7 @@ class Plugin extends Base
     }
     public function getPluginVersion()
     {
-        return '1.0.2';
+        return '1.0.3';
     }
     public function getPluginHomepage()
     {
