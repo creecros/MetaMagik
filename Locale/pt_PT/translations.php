3376d59ea4d0fd2cd419e9b9d7ebacbb17e1cc18
Eskiso <eSkiSo@users.noreply.github.com>
2016-08-01 15:09:19 +0100

    Created Translation to pt_PT (#23)

---
+++ Locale/pt_PT/translations.php
@@ -0,0 +1,11 @@
+<?php
+
+return array(
+    'Metadata' => 'Metadados',
+    'Key' => 'Chave',
+    'Value' => 'Valor',
+    'No metadata' => 'Sem metadados',
+    'Remove Metadata' => 'Remover Metadados',
+    'Do you really want to remove this metadata?' => 'Tem a certeza que pretende remover estes metadados?',
+    'Add Metadata' => 'Adicionar Metadados',
+);
