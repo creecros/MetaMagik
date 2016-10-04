1af0bc3f59ee364da889859ac30cf9b1196302b7
Dmitry <kochevd@gmail.com>
2016-10-04 11:10:35 +0300

    Create ru_RU translations.php

---
+++ Locale/ru_RU/translations.php
@@ -0,0 +1,10 @@
+<?php
+return array(
+    'Metadata' => 'Метаданные',
+    'Key' => 'Ключ',
+    'Value' => 'Значение',
+    'No metadata' => 'Нет метаданных',
+    'Remove Metadata' => 'Удалить метаданные',
+    'Do you really want to remove this metadata?' => 'Вы действительно хотить удалить эти метаданные?',
+    'Add Metadata' => 'Добавить метаданные',
+);
