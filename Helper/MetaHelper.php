2f9fb8fb8b5f5513924236991c27a159d387995d
creecros <creecros@gmail.com>
2018-09-11 13:41:52 -0400

    Update and rename TaskHelper.php to MetaHelper.php

---
+++ Helper/MetaHelper.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Kanboard\Plugin\MetaMagik\Helper;
+
+use Kanboard\Core\Base;
+
+/**
+ * Meta helper
+ *
+ */
+class MetaHelper extends Base
+{
+
+    public function renderMetaFields(array $values)
+    {
+        $metadata = $this->taskMetadataModel->getAll($values['id']);
+        foreach ($metadata as $key => $value) {
+        $html .= $this->helper->form->label(t($key), $key);
+        $html .= $this->helper->form->text($key, $value, $errors, $attributes, 'form-input-small');
+        }
+
+        return $html;
+    }
+
+}
