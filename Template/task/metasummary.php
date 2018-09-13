d3693172951d31ae6854ef3fb99f5214b7560e65
creecros <creecros@gmail.com>
2018-09-12 20:56:48 -0400

    Update metasummary.php

--- Template/task/metasummary.php
+++ Template/task/metasummary.php
@@ -1,6 +1,3 @@
-<div class="page-header">
-    <h2><?= t('Metadata') ?></h2>
-</div>
 
 
 <?php 
@@ -9,29 +6,12 @@ if (empty($metadata)):
 ?>
     <p class="alert"><?= t('No metadata') ?></p>
 <?php else: ?>
-<div class="meta-summary-container color-<?= $task['color_id'] ?>">
-    <table class="table-small table-fixed">
-    <tr>
-        <th class="column-40"><?= t('Key') ?></th>
-        <th class="column-40"><?= t('Value') ?></th>
-        <th class="column-20"><?= t('Action') ?></th>
-    </tr>
-    <?php foreach ($metadata as $key => $value): ?>
-    <tr>
-        <td><?= $key ?></td>
-        <td><?= $value ?></td>
-        <td>
-            <ul>
-                <li>
-                    <?= $this->url->link(t('Remove'), 'MetadataController', 'confirmTask', ['plugin' => 'metaMagik', 'task_id' => $task['id'], 'project_id' => $project['id'], 'key' => $key], false, 'popover') ?>
-                </li>
-                <li>
-                    <?= $this->url->link(t('Edit'), 'MetadataController', 'editTask', ['plugin' => 'metaMagik', 'task_id' => $task['id'], 'project_id' => $project['id'], 'key' => $key], false, 'popover') ?>
-                </li>
-            </ul>
-        </td>
-    </tr>
+<button class="accordion"><?= t('Section 1') ?></button>
+<div class="panel">
+  <?php foreach ($metadata as $key => $value): ?>
+    <p><?= $key ?></p>
+    <p><?= $value ?></p>
     <?php endforeach ?>
-    </table>
 </div>
+
 <?php endif ?>
