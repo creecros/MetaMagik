f332461b18713d2ae7130ad8669e29a81b9866f0
creecros <creecros@gmail.com>
2018-09-12 21:36:59 -0400

    Update metasummary.php

--- Template/task/metasummary.php
+++ Template/task/metasummary.php
@@ -10,23 +10,10 @@ if (empty($metadata)):
         <h3><a href="#" class="fa accordion-toggle"></a> <?= t('MetaMagik') ?></h3>
     </div>
 <div class="accordion-content">
-       <div class="task-summary-container color-<?= $task['color_id'] ?>">
-        <div class="task-summary-columns">
-            <div class="task-summary-column">
-  <?php 
-        $rows = 0;
-        foreach ($metadata as $key => $value): 
-        $rows += 1; 
-  ?>
-        <?php if ($rows <= 3): ?>
+  <?php foreach ($metadata as $key => $value): ?>
         <p><strong><?= $key ?><?= t(': ') ?></strong><?= $value ?></p>
-        <?php else: ?>
-                </div>
-                <div class="task-summary-column">
-                        <p><strong><?= $key ?><?= t(': ') ?></strong><?= $value ?></p>
     <?php endforeach ?>
-     </div>
-   </div>
+
 </div>
 </section>
 <?php endif ?>
