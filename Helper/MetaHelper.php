c0cb1ec16ff9a1db18670c34f2f449c98f6207d9
creecros <creecros@gmail.com>
2018-09-13 11:45:05 -0400

    Update MetaHelper.php

--- Helper/MetaHelper.php
+++ Helper/MetaHelper.php
@@ -109,7 +109,6 @@ class MetaHelper extends Base
              foreach ($opt_explode as $name => $value) {
                             $meta_opt[$value] = $value;
              }
-             $values['metamagikkey_' . $key] = implode(',', $values['metamagikkey_' . $key]);
              $html .= $this->helper->form->label($key, 'metamagikkey_' . $key);
              $html .= $this->helper->form->checkboxes('metamagikkey_' . $key, $meta_opt, $values); 
              $meta_opt = array();
