6b5039d581f29a5e7ea891b0921e1d2613b657cc
creecros <creecros@gmail.com>
2018-09-12 22:17:21 -0400

    Update Sqlite.php

--- Schema/Sqlite.php
+++ Schema/Sqlite.php
@@ -4,7 +4,13 @@ namespace Kanboard\Plugin\MetaMagik\Schema;
 
 use PDO;
 
-const VERSION = 1;
+const VERSION = 2;
+
+function version_2(PDO $pdo)
+{
+    $pdo->exec('ALTER TABLE metadata_types ADD COLUMN options VARCHAR(255)');
+}
+
 
 function version_1(PDO $pdo)
 {
@@ -14,7 +20,6 @@ function version_1(PDO $pdo)
           human_name VARCHAR(255) NOT NULL,
           machine_name VARCHAR(255) NOT NULL,
           data_type VARCHAR(50) NOT NULL,
-          options VARCHAR(255) DEFAULT '',
           is_required BOOLEAN DEFAULT 0,
           attached_to VARCHAR(50) NOT NULL,
           UNIQUE(machine_name, attached_to)
