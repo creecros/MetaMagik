3696b5ccb410fbff911f9934e45df5259c8e9e1d
creecros <creecros@gmail.com>
2018-09-19 20:20:06 -0400

    Update sidebar.php

--- Template/config/sidebar.php
+++ Template/config/sidebar.php
@@ -1,3 +1,3 @@
 <li <?= $this->app->checkMenuSelection('MetadataTypes', 'config', 'Metadata') ?>>
-    <?= $this->url->link(t('Metadata Types'), 'MetadataTypesController', 'config', ['plugin' => 'MetaMagik']) ?>
+    <?= $this->url->link(t('Custom Fields'), 'MetadataTypesController', 'config', ['plugin' => 'MetaMagik']) ?>
 </li>
