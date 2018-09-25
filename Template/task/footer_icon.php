dcdd368e2014b2f3b871aac67abafadfd454eb92
Craig Crosby <creecros@gmail.com>
2018-09-25 11:41:09 -0400

    Update footer_icon.php
    Only count non empty keys

--- Template/task/footer_icon.php
+++ Template/task/footer_icon.php
@@ -3,6 +3,6 @@ $metadata = $this->task->taskMetadataModel->getAll($task['id']);
 if (!empty($metadata)):
 ?>
         <span title="<?= t('Metadata') ?>" class="tooltip" data-href="<?= $this->url->href('MetadataController', 'task_footer', ['plugin' => 'metaMagik', 'task_id' => $task['id'], 'project_id' => $task['project_id']]) ?>">
-            <i class="fa fa-plus-square-o fa-fw"></i><?= count($metadata) ?>
+            <i class="fa fa-plus-square-o fa-fw"></i><?= count(array_filter($metadata)) ?>
         </span>
 <?php endif ?>
