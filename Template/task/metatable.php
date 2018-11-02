26fea1a6c44e9e87805927e059621ca7c0d1360f
Craig Crosby <creecros@gmail.com>
2018-11-02 14:53:47 -0400

    Create metatable.php

---
+++ Template/task/metatable.php
@@ -0,0 +1,21 @@
+<table
+       class="metadata-table table-striped table-scrolling"
+       data-save-position-url="<?= $this->url->href('MetadataTypesController', 'movePosition', array('task_id' => $task['id'])) ?>"
+>
+<thead>
+        <tr>
+          <th><?= t('Custom Field') ?></th>
+          <th><?= t('Value') ?></th>
+        </tr>
+</thead>
+<tbody>
+  <?php foreach ($custom_fields as $custom_field): ?>
+        <?php if (!empty($this->task->taskMetadataModel->get($task['id'], $custom_field['human_name'], ''))): ?>
+        <tr data-id="<?= $custom_field['id'] ?>">
+                  <td><i class="fa fa-arrows-alt draggable-row-handle ui-sortable-handle" title="Change metadata position"></i>&nbsp;<strong><?= $custom_field['human_name'] ?></strong></td>
+                  <td><?= $this->task->taskMetadataModel->get($task['id'], $custom_field['human_name'], '') ?></td>
+        </tr>
+        <?php endif ?>
+    <?php endforeach ?>
+</tbody>
+</table>
