0defc895062b6015fd9c87d0743c6a5374953b3e
Craig Crosby <creecros@gmail.com>
2018-11-02 14:53:29 -0400

    Update metasummary.php

--- Template/task/metasummary.php
+++ Template/task/metasummary.php
@@ -1,6 +1,8 @@
 
 <?php 
+$metadata = $this->task->taskMetadataModel->getAll($task['id']);
 $custom_fields = $this->task->metadataTypeModel->getAll();
+if ($_SESSION['user']['role'] !== 'app-admin') { $edits = false; }
 if (empty($custom_fields)): 
 ?>
 <?php else: ?>
@@ -9,27 +11,11 @@ if (empty($custom_fields)):
         <h3><a href="#" class="fa accordion-toggle"></a> <?= t('MetaMagik') ?></h3>
     </div>
 <div class="accordion-content">
-<table
-       class="metadata-table table-striped table-scrolling"
-       data-save-position-url="<?= $this->url->href('MetadataTypesController', 'movePosition', array('task_id' => $task['id'])) ?>"
->
-<thead>
-        <tr>
-          <th><?= t('Custom Field') ?></th>
-          <th><?= t('Value') ?></th>
-        </tr>
-</thead>
-<tbody>
-  <?php foreach ($custom_fields as $custom_field): ?>
-        <?php if (!empty($this->task->taskMetadataModel->get($task['id'], $custom_field['human_name'], ''))): ?>
-        <tr data-id="<?= $custom_field['id'] ?>">
-                  <td><i class="fa fa-arrows-alt draggable-row-handle ui-sortable-handle" title="Change metadata position"></i>&nbsp;<strong><?= $custom_field['human_name'] ?></strong></td>
-                  <td><?= $this->task->taskMetadataModel->get($task['id'], $custom_field['human_name'], '') ?></td>
-        </tr>
-        <?php endif ?>
-    <?php endforeach ?>
-</tbody>
-</table>
+        <?= $this->render('metamagik:task/metatable', array(
+            'custom_fields' => $custom_fields,
+            'task' => $task,
+            'editable' => $edits
+        )) ?>
 </div>
 
 </section>
