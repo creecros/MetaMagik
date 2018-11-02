720be98d8ed4c86e2422d18efcfb67fb8895ba71
Craig Crosby <creecros@gmail.com>
2018-11-02 14:18:19 -0400

    Update MetadataTypeModel.php

--- Model/MetadataTypeModel.php
+++ Model/MetadataTypeModel.php
@@ -38,4 +38,29 @@ class MetadataTypeModel extends Base
             ->eq('id', $id)
             ->remove();
     }
+
+    public function changePosition($id, $position)
+    {
+        if ($position < 1 || $position > $this->db->table(self::TABLE)->count()) {
+            return false;
+        }
+
+        $ids = $this->db->table(self::TABLE)->neq('id', $id)->asc('position')->findAllByColumn('id');
+        $offset = 1;
+        $results = array();
+
+        foreach ($ids as $current_id) {
+            if ($offset == $position) {
+                $offset++;
+            }
+
+            $results[] = $this->db->table(self::TABLE)->eq('id', $current_id)->update(array('position' => $offset));
+            $offset++;
+        }
+
+        $results[] = $this->db->table(self::TABLE)->eq('id', $id)->update(array('position' => $position));
+
+        return !in_array(false, $results, true);
+    }
+    
 }
