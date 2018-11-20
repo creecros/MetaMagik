9f3121eb67ca56d8e14c0bca26565a7a284cea58
Craig Crosby <creecros@gmail.com>
2018-11-19 19:38:18 -0500

    Update MetaHelper.php

--- Helper/MetaHelper.php
+++ Helper/MetaHelper.php
@@ -88,7 +88,7 @@ class MetaHelper extends Base
         foreach ($metasettings as $setting) {
             $key = $setting['human_name'];
             if (isset($values['id']) && $setting['data_type'] !== 'check') {
-                if (isset($metadata['key'])) { $values['metamagikkey_' . $key] = $metadata[$key] }
+                if (isset($metadata['key'])) { $values['metamagikkey_' . $key] = $metadata[$key]; }
             } elseif (isset($values['id']) && $setting['data_type'] == 'check') {
                 if (isset($metadata['key'])) {
                     $wtf = explode(',', $metadata[$key]);
