53c2100d1b9f42b558988c310b7ed70875670d90
Craig Crosby <creecros@gmail.com>
2018-11-20 12:30:21 -0500

    Update MetadataTypesController.php

--- Controller/MetadataTypesController.php
+++ Controller/MetadataTypesController.php
@@ -123,7 +123,7 @@ class MetadataTypesController extends BaseController
 
         if (! empty($values) && $_SESSION['user']['role'] == 'app-admin') {
             $result = $this->metadataTypeModel->changePosition($values['id'], $values['position'], $values['columnnumber']);
-            if ($results[count($results) + 1] == 'abc is true') { 
+            if ($result[count($result) + 1] == 'abc is true') { 
                 $abc = true; 
                 array_pop($result);
             } else { 
