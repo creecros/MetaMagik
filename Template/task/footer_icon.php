954ce834045472f4d4158f5762540c8396f54059
Craig Crosby <creecros@gmail.com>
2018-12-07 08:58:18 -0500

    Update footer_icon.php

--- Template/task/footer_icon.php
+++ Template/task/footer_icon.php
@@ -1,4 +1,3 @@
-<?php if ($this->user->hasProjectAccess('metadata', 'index', $task['project_id'])): ?>
 <?php 
 $metadata = $this->task->taskMetadataModel->getAll($task['id']);
 if (count(array_filter($metadata)) > 0):
@@ -6,5 +5,4 @@ if (count(array_filter($metadata)) > 0):
         <span title="<?= t('Metadata') ?>" class="tooltip" data-href="<?= $this->url->href('MetadataController', 'task_footer', ['plugin' => 'metaMagik', 'task_id' => $task['id'], 'project_id' => $task['project_id']]) ?>">
             <i class="fa fa-plus-square-o fa-fw"></i><?= count(array_filter($metadata)) ?>
         </span>
-<?php endif ?>
 <?php endif ?>
