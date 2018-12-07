7b7333be0b42a5ca7c7fb25f94eb319696b45ec8
Craig Crosby <creecros@gmail.com>
2018-12-07 08:58:32 -0500

    Update sidebar.php

--- Template/task/sidebar.php
+++ Template/task/sidebar.php
@@ -1,8 +1,6 @@
-<?php if ($this->user->hasProjectAccess('metadata', 'index', $task['project_id'])): ?>
     <ul>
         <li>
         <i class="fa fa-plus-square-o fa-fw"></i>
         <?= $this->url->link(t('Metadata'), 'MetadataController', 'task', ['plugin' => 'metaMagik', 'task_id' => $task['id'], 'project_id' => $task['project_id']]) ?>
         </li>
     </ul>
-<?php endif ?>
