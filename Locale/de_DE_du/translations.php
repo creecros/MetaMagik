4892b0e669d7536c32bc0d83059252f032881903
Manfred Hoffmann <48651533+manne65-hd@users.noreply.github.com>
2021-04-09 13:47:47 +0200

    Added Translation "Deutsch (du)"

---
+++ Locale/de_DE_du/translations.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'Metadata'                                    => 'Metadaten',
+    'Key'                                         => 'Schlüssel',
+    'Value'                                       => 'Wert',
+    'No metadata'                                 => 'keine Metadaten vorhanden',
+    'Remove Metadata'                             => 'Metadaten löschen',
+    'Do you really want to remove this metadata?' => 'Willst du diese Metadaten wirklich löschen?',
+    'Add Metadata'                                => 'Metadaten hinzufügen',
+    'Custom Fields'                               => 'Benutzerdefinierte Felder', 
+    'Remove Metadata Task Field'                  => 'Benutzerdefiniertes Feld entfernen', 
+    'Options - comma seperated list for dropdown, radio, or checkbox group. 255 chars max.' => 'Optionen - kommagetrennte Liste für Dropdown-Liste, Radio oder Checkbox-Gruppe. 255 Zeichen max.',
+    'Attached Entity'                             => 'Angehängte Entität',
+    'No types have been defined yet.'             => 'Es wurden noch keine Typen definiert.',
+    'Field Name'                                  => 'Feldname',
+    'Type'                                        => 'Typen',
+    'Options'                                     => 'Optionen',
+    'Action'                                      => 'Aktion',
+];
