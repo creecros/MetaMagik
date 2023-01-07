1a78f40734f9bdae42083200e4a3392c88ae29bb
JustFxDev <fx@justmail.de>
2023-01-07 20:20:23 +0100

    Added class name to icon and counter on task

--- Template/task/footer_icon.php
+++ Template/task/footer_icon.php
@@ -1,7 +1,8 @@
 <?php
 if ($task['nb_metadata'] > 0):
 ?>
-        <span title="<?= t('Metadata') ?>" class="tooltip" data-href="<?= $this->url->href('MetadataController', 'task_footer', ['plugin' => 'metaMagik', 'task_id' => $task['id'], 'project_id' => $task['project_id']]) ?>">
+        <span title="<?= t('Metadata') ?>" class="tooltip metamagik-icon-count" data-href="<?= $this->url->href('MetadataController', 'task_footer', ['plugin' => 'metaMagik', 'task_id' => $task['id'], 'project_id' => $task['project_id']]) ?>">
             <i class="fa fa-plus-square-o fa-fw"></i><?= $task['nb_metadata'] ?>
         </span>
 <?php endif ?>
+
