b9c889dc3015b49a5871b351d07010d72005021c
Dmytro Voytko <dmytro.voytko@gmail.com>
2023-10-14 10:59:55 +0300

    Add Ukrainian translation

---
+++ Locale/uk_UA/translations.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    'Metadata'                                    => 'Метадані',
+    'Key'                                         => 'ключ',
+    'Value'                                       => 'Значення',
+    'No metadata'                                 => 'Без метаданих',
+    'Remove Metadata'                             => 'Видалити метадані',
+    'Do you really want to remove this metadata?' => 'Ви справді хочете видалити ці метадані?',
+    'Add Metadata'                                => 'Додати метадані',
+    'Custom Fields'                               => 'Настроювані поля', 
+    'Remove Metadata Task Field'                  => 'Видалити поле метаданих завдання', 
+    'Options - comma seperated list for dropdown, radio, or checkbox group. 255 chars max.' => 'Параметри – розділений комами список для розкривного меню, перемикача або групи прапорців. Макс. 255 символів.',
+    'Attached Entity'                             => 'Прикріплений об\'єкт',
+    'No types have been defined yet.'             => 'Типи ще не визначені.',
+    'Field Name'                                  => 'Назва поля',
+    'Type'                                        => 'Тип',
+    'Options'                                     => 'Опції',
+    'Action'                                      => 'Дія',
+	'Custom field total distribution for open tasks' => 'Загальний розподіл настроюваних полів для відкритих завдань',
+	'Custom field total distribution with date range' => 'Загальний розподіл настроюваних полів з діапазоном дат',
+];
